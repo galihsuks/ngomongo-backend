c13ae0c9e49599aa50b9a642dd84f5617563369b
galihsuks <galih8.4.2001@gmail.com>
2024-10-29 22:31:33 +0700

    ganti port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 8080;
+const port = 8081;
 const mongoose = require("mongoose");
 const roomRoute = require("./routes/room.route.js");
 const userRoute = require("./routes/user.route.js");
